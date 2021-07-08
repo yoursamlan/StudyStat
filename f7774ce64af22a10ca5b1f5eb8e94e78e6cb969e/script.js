5ef45ff6cf56f235f4304c13c0cb3a6379cae4bc
yoursamlan <skamlan99@gmail.com>
2021-07-08 19:16:17 +0530

    Application

---
+++ f7774ce64af22a10ca5b1f5eb8e94e78e6cb969e/script.js
@@ -0,0 +1,68 @@
+var hist = ["Overall"];
+
+function ch0() {
+	document.getElementById("hmframe").src = "index0.html";
+	document.getElementById(hist[hist.length - 1]).style.cssText='color : "#999";fontWeight : "500";background : "none";';
+	document.getElementById("Overall").style.color = "#fff";
+	document.getElementById("Overall").style.fontWeight = "900";
+	document.getElementById("Overall").style.background = "#375b13";
+	hist.push("Overall");
+}
+
+function ch1() {
+	document.getElementById("hmframe").src = "index1.html";
+	document.getElementById(hist[hist.length - 1]).style.cssText='color : "#999";fontWeight : "500";background : "none";';
+	document.getElementById("History").style.color = "#fff";
+	document.getElementById("History").style.fontWeight = "900";
+	document.getElementById("History").style.background = "#5d2c10";
+	hist.push("History");
+}
+function ch2() {
+	document.getElementById("hmframe").src = "index2.html";
+	document.getElementById(hist[hist.length - 1]).style.cssText='color : "#999";fontWeight : "500";background : "none";';
+	document.getElementById("Geography").style.color = "#fff";
+	document.getElementById("Geography").style.fontWeight = "900";
+	document.getElementById("Geography").style.background = "#173e56";
+	hist.push("Geography");
+}
+function ch3() {
+	document.getElementById("hmframe").src = "index3.html";
+	document.getElementById(hist[hist.length - 1]).style.cssText='color : "#999";fontWeight : "500";background : "none";';
+	document.getElementById("Polity").style.color = "#fff";
+	document.getElementById("Polity").style.fontWeight = "900";
+	document.getElementById("Polity").style.background = "#37135b";
+	hist.push("Polity");
+}
+function ch4() {
+	document.getElementById("hmframe").src = "index4.html";
+	document.getElementById(hist[hist.length - 1]).style.cssText='color : "#999";fontWeight : "500";background : "none";';
+	document.getElementById("Economics").style.color = "#fff";
+	document.getElementById("Economics").style.fontWeight = "900";
+	document.getElementById("Economics").style.background = "#482536";
+	hist.push("Economics");
+}
+function ch5() {
+	document.getElementById("hmframe").src = "index5.html";
+	document.getElementById(hist[hist.length - 1]).style.cssText='color : "#999";fontWeight : "500";background : "none";';
+	document.getElementById("Reasoning").style.color = "#fff";
+	document.getElementById("Reasoning").style.fontWeight = "900";
+	document.getElementById("Reasoning").style.background = "#01426d";
+	hist.push("Reasoning");
+}
+function ch6() {
+	document.getElementById("hmframe").src = "index6.html";
+	document.getElementById(hist[hist.length - 1]).style.cssText='color : "#999";fontWeight : "500";background : "none";';
+	document.getElementById("English").style.color = "#fff";
+	document.getElementById("English").style.fontWeight = "900";
+	document.getElementById("English").style.background = "#615c0c";
+	hist.push("English");
+}
+function ch7() {
+	document.getElementById("hmframe").src = "index7.html";
+	document.getElementById(hist[hist.length - 1]).style.cssText='color : "#999";fontWeight : "500";background : "none";';
+	document.getElementById("Science").style.color = "#fff";
+	document.getElementById("Science").style.fontWeight = "900";
+	document.getElementById("Science").style.background = "#610c61";
+	hist.push("Science");
+}
+
